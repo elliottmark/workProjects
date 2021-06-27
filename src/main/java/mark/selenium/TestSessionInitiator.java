9adc905b072f99ac29c93b0887cf0e700cd7ecf6
Mark Elliott <neo1491@msn.com>
2021-06-27 09:06:16 -0400

    Update TestSessionInitiator.java

--- src/main/java/mark/selenium/TestSessionInitiator.java
+++ src/main/java/mark/selenium/TestSessionInitiator.java
@@ -1,7 +1,7 @@
 package mark.selenium;
 
 import java.util.concurrent.TimeUnit;
-
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
